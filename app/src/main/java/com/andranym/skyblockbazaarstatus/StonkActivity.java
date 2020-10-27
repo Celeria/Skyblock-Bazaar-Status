4210f041f9db3a6fe3aee2745cdb28898dd2a98d
Patrick Meng <patm314@gmail.com>
2020-10-26 22:18:55 -0500

    added beginning of stonk challenge

--- app/src/main/java/com/andranym/skyblockbazaarstatus/StonkActivity.java
+++ app/src/main/java/com/andranym/skyblockbazaarstatus/StonkActivity.java
@@ -167,6 +167,7 @@ public class StonkActivity extends AppCompatActivity {
                 for(Stonk stonk:displayedStonks) {
                     if (stonk.getProductName().equals(newProduct)) {
                         repeat = true;
+                        break;
                     }
                 }
                 if (!repeat) {
