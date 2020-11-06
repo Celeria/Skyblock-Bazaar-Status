05074875b63408993330b34434ff4d89c38d8b1f
Patrick Meng <patm314@gmail.com>
2020-11-06 11:05:53 -0600

    adjusted mobile data warning message location. Fixed obscure multiplier bug.

--- app/src/main/java/com/andranym/skyblockbazaarstatus/MainActivity.java
+++ app/src/main/java/com/andranym/skyblockbazaarstatus/MainActivity.java
@@ -420,19 +420,19 @@ public class MainActivity extends AppCompatActivity {
         //endregion
 
         //region Block people from stealing this app
-//        boolean usedBefore = sharedPref.getBoolean("copyWriteDetection",false);
-//        if (!usedBefore) {
-//            SharedPreferences.Editor editor = sharedPref.edit();
-//            editor.putBoolean("copyWriteDetection",true);
-//            editor.putLong("timeAccessed",System.currentTimeMillis());
-//            editor.commit();
-//        } else {
-//            long timeAccessed = sharedPref.getLong("timeAccessed",0);
-//            if ((double)(System.currentTimeMillis() - timeAccessed)/1000/60/60/24 > 3) {
-//                Intent blockUser = new Intent(this,BannedActivity.class);
-//                startActivity(blockUser);
-//            }
-//        }
+        boolean usedBefore = sharedPref.getBoolean("copyWriteDetection",false);
+        if (!usedBefore) {
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putBoolean("copyWriteDetection",true);
+            editor.putLong("timeAccessed",System.currentTimeMillis());
+            editor.commit();
+        } else {
+            long timeAccessed = sharedPref.getLong("timeAccessed",0);
+            if ((double)(System.currentTimeMillis() - timeAccessed)/1000/60/60/24 > 3) {
+                Intent blockUser = new Intent(this,BannedActivity.class);
+                startActivity(blockUser);
+            }
+        }
         //endregion
 
         //region Displays how long its been since the data was last updated
